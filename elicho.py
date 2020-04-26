1ffe9fff462f54b4ea0ba3da677993f5cd3d186b
Eliyas Girma <40254049+elicho99@users.noreply.github.com>
2020-04-26 10:47:53 +0300

    trying to deploy with heroku

--- elicho.py
+++ elicho.py
@@ -7,7 +7,23 @@ import os
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 
-eli = Database()
+mode = os.getenv("MODE")
+TOKEN = os.getenv("TOKEN")
+if mode == "dev":
+    def run(updater):
+        updater.start_polling()
+elif mode == "prod":
+    def run(updater):
+        PORT = int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+else:
+    logger.error("No MODE specified!")
+    sys.exit(1)
 
 
 def start(bot, update):
@@ -106,7 +122,7 @@ def echo(bot, update):
 
 
 def main():
-    updater = Updater("634656340:AAFL43JVLRzmLdwDFcqkw4jC1gN1l1UTeHg")
+    updater = Updater(TOKEN)
 
     dp = updater.dispatcher
 
