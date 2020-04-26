7eeb07f87137353f72b3b48ce219441d99a733cf
Eliyas <beliyas4@gmail.com>
2020-04-26 14:03:41 +0300

    file_sichemerbet

--- elicho.py
+++ elicho.py
@@ -28,67 +28,14 @@ else:
 
 SECTION,Fname,ID ,LAST= range(4)
 
-info = []
+
 def start(update, context):
     user = update.message.from_user
     user_name = str(user.first_name)
-    update.message.reply_text("Hello "+user_name +"  /register yourself Please\n"
-    'Give accurate information about yourself')
-
-
-def register(update, context):
-    #eli = Database()
-    chat_id = update.message.chat_id
-    user = update.message.from_user
-    user_name = str(user.first_name)
-    #eli.add_user(chat_id, user_name)
-    #update.message.reply_text('Registered!')
-    
-    reply_keyboard = [['A', 'B', 'C','ADD']]
-    
-    
-    update.message.reply_text(
-        'Hi! '+user_name+'. I will ask you some questions.\n\n '
-        'Which section are you in?',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    
-    return Fname
+    update.message.reply_text("Hello "+user_name+" send your assignment.")
 
 
 
-
-
-
-def Full_Name(update, context):
-    user = update.message.from_user
-    sec = str(update.message.text)
-    info.append(sec)
-    print("Your section is "+sec)
-    #print("full name of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Enter your full name')
-
-    return ID
-
-def id_number(update, context):
-    user = update.message.from_user
-    Fullname = str(update.message.text)
-    info.append(Fullname)
-    #print("id of %s: %s", user.first_name, update.message.text)
-    print("full name is "+Fullname)
-    update.message.reply_text('Enter your full iD')
-  
-    return LAST
-
-
-def LAST(update, context):
-    user = update.message.from_user
-    stud_id = str(update.message.text)
-    info.append(stud_id)
-    print(" your id is"+stud_id)
-    update.message.reply_text('registered')
-    
-    print(info)
-    return ConversationHandler.END
 
 
 def ass_doc(bot, update):
@@ -118,22 +65,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('register', register)],
-
-        states={
-            SECTION: [MessageHandler(Filters.regex('^(A|B|C|ADD)$'), register)],
-
-            Fname: [MessageHandler(Filters.text,Full_Name)],
-
-            ID: [MessageHandler(Filters.text, id_number)],
-            LAST: [MessageHandler(Filters.text, LAST)]
-                    },
-
-        fallbacks=[CommandHandler('cancel', help)]
-    )
-
-    dp.add_handler(conv_handler)
+    
     
  
     dp.add_handler(MessageHandler(Filters.text, echo))
